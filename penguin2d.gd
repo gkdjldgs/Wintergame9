8c87ecdf3553472182693de3fab8cba37d5ab936
melon <jermaise@gmail.com>
2025-11-27 19:11:40 -0700

    Penguin
    I corrected the images, they were offset the up and down ones because I didn't crop the image correctly.

--- penguin2d.gd
+++ penguin2d.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
-var speed = 45
+@export var speed = 120
+@onready var player = $Sprite2D2/AnimatedSprite2d
 
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "forward", "back")
+	velocity = input_direction * speed
 
-func _physics_process(delta: float) -> void:
-	var strafe = Vector2(Input.get_axis('left',"right",),
-	Input.get_axis('forward','back'))
-
-	if strafe != Vector2.ZERO:
-		velocity = speed * strafe.normalized()
-		
+	if Input.is_action_pressed('left'):
+		player.play('left')
+	elif Input.is_action_pressed("right"):
+		player.play("right")
+	elif Input.is_action_pressed("forward"):
+		player.play("backwards")
+	elif Input.is_action_pressed('back'):
+		player.play("forward")
 	else:
-		velocity.x = move_toward(velocity.x,0,speed)
-		velocity.y = move_toward(velocity.y,0,speed)
-		
+		pass
+func _physics_process(delta):
+	get_input()
 	move_and_slide()
 	
