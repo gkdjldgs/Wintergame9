a3db605916f0ed7bcd3aa401e2eea7277ad4054a
melon <jermaise@gmail.com>
2025-11-29 17:53:49 -0700

    FISHING??
    Out of all things I wanted to work on this week, fishing was not what I expected.
    Thinking however and implimenting are completely different and it is difficult for me, luckly I'm following the classic tutorial on implemeting a fishing system by 16BitDev

---
+++ fishinggame.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+@onready var control := %Control
+@onready var Secondary := %Secondary
+@onready var fishground := %fishground
+@onready var playercube := %playercube
+@onready var Progress := %ProgressBar
+@onready var fish1 := %Sunfish
+@onready var label := %Label
+@onready var particles :=%GPUParticles2D
---
+++ new_script.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name Fishtype
+
+@export var fish_name: String = 'Sunfish'
+@export_range(0,100,1) var base_price: int = 10
+@export_range(0,150,1) var lengthincm: float = 20
+@export var tint: Color = Color.SKY_BLUE
+
+@export_range(0.2, 3.0, 0.05) var bite_time_average: float = 1.22
+@export_range(0.1, 2.0, 0.05) var bite_time_dif: float = 0.5
+
+@export_range(0.0, 1.0, 0.01) var difficulty: float = 0.45
